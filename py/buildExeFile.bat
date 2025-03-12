162095fb639a1a55e9bd0b69f07a9c7d6535fb4a
yox <vivien.hoyaux@gmail.com>
2025-03-12 16:35:08 +0100

    fixe ammo SOST and RFLP

--- py/buildExeFile.bat
+++ py/buildExeFile.bat
@@ -4,6 +4,6 @@ echo  Compilation avec PyInstaller...
 
 pyinstaller --onefile --clean --name ModParameters --icon=Images/ModParameters.ico --distpath . main/main.py
 
-echo py -3.12 -m nuitka --standalone --assume-yes-for-downloads --nofollow-import-to=tkinter --nofollow-import-to=PIL main.py
+:: py -3.12 -m nuitka --standalone --assume-yes-for-downloads --nofollow-import-to=tkinter --nofollow-import-to=PIL main.py
 echo  Compilation terminée !
 pause
